3b04d250188db1b14d9cb188c5d6499654fd46eb
Caumannerman <uigohiiming2@gmail.com>
2021-12-04 00:01:58 +0900

    Revise&Test

--- AI_#8-1_GradientDescent.py
+++ AI_#8-1_GradientDescent.py
@@ -1,13 +1,13 @@
-x = 10
-learning_rate = 0.01
+x = 2
+learning_rate = 0.05
 precision = 0.00001
-max_iterations = 100
+max_iterations = 500
 
 #손실 함수를 람다식으로 정의한다.
-loss_func = lambda x: (x-3)**2 + 10
+loss_func = lambda x: (x-6)**2 + 10
 
 #그래디언트를 람다식으로 정의한다. 손실함수의 1차 미분값이다.
-gradient = lambda x: 2*x - 6
+gradient = lambda x: 2*x - 12
 
 #그래디언트 강하법
 for i in range(max_iterations):
--- AI_#8-3_tf&keras1.py
+++ AI_#8-3_tf&keras1.py
@@ -7,11 +7,12 @@ X = data = np.linspace(1,2,200) #시작값=1, 종료값=2, 개수=200
 y = X*4 + np.random.rand(200) * 0.3  #x를 4배로 하고 편차 0.3정도의 가우시안 잡음추가
 
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Dense(1, input_dim=1, activation='linear'))
+model.add(tf.keras.layers.Dense(2, input_dim=1, activation='linear'))
 model.compile(optimizer='sgd', loss='mse', metrics=['mse'])
 model.fit(X, y, batch_size=1, epochs=30)
 
+model.summary()
 predict = model.predict(data)
 
-plt.plot(data, predict, 'b', data, y, 'k. ') #첫 번째 그래프는 파란색 마커로
+plt.plot(data, predict, 'b.', data, y, 'k.') #첫 번째 그래프는 파란색 마커로
 plt.show()
